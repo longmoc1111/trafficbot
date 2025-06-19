36f359e0d99b2c6cae1b641dd72b6c2d01eceda3
Nguyễn Chước Long <longmoc93@gmail.com>
2025-06-20 00:58:59 +0700

    update signage

--- app/Models/SignageType.php
+++ app/Models/SignageType.php
@@ -3,13 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Models\signage;
+use App\Models\Signage;
 class SignageType extends Model
 {
     protected $primaryKey = "SignageTypeID";
     protected $fillable = ["SignageTypeID","SignagesTypeName","SignagesTypeDescription"];
     public function signageType_signage(){
-        return $this->hasMany(signage::class, "SignageTypeID","SignageTypeID");
+        return $this->hasMany(Signage::class, "SignageTypeID","SignageTypeID");
     }
 
 }
